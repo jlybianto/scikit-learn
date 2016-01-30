93e58c09cd1903fc67abb8db6660b1beb8765a31
jlybianto <jesse.lybianto@gmail.com>
2016-01-30 03:00:29 -0700

    Add background information and import packages

---
+++ model-selection-and-evaluation/cross-validation.py
@@ -0,0 +1,19 @@
+# ----------------
+# BACKGROUND INFORMATION
+# ----------------
+
+'''Learning the parameters of a prediction function and testing it on the same data set is a methodological
+mistake: a model would just repeat the labels of the samples that it has just seen would have a perfect
+score but would fail to predict anything useful on yet-unseen data. This situation is called overfitting.'''
+
+'''To avoid it, it is common practice when performing a (supervised) machine learning experiment to hold out
+part of the available data as a test set (X_test, y_test).'''
+
+# ----------------
+# IMPORT PACKAGES
+# ----------------
+
+import numpy as numpy
+from sklearn import cross_validation
+from sklearn import datasets
+from sklearn import svm
