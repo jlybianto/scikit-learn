5470f7e2fa0339491081a0df6719c7f836d3a2f4
jlybianto <jesse.lybianto@gmail.com>
2016-02-07 15:57:36 +0700

    Add cross-validation and K-Fold

--- model-selection-and-evaluation/cross-validation.py
+++ model-selection-and-evaluation/cross-validation.py
@@ -13,10 +13,11 @@ part of the available data as a test set (X_test, y_test).'''
 # IMPORT PACKAGES
 # ----------------
 
-import numpy as numpy
+import numpy as np
 from sklearn import cross_validation
 from sklearn import datasets
 from sklearn import svm
+from sklearn.cross_validation import KFold
 
 # ----------------
 # OBTAIN DATA
@@ -33,3 +34,24 @@ X_train, X_test, y_train, y_test = cross_validation.train_test_split(
 	iris.data, iris.target, test_size=0.4, random_state=0)
 
 clf = svm.SVC(kernel="linear", C=1).fit(X_train, y_train)
+clf.score(X_test, y_test)
+
+# ----------------
+# COMPUTING CROSS-VALIDATED METRICS
+# ----------------
+
+# Estimate the accuracy of a linear kernel support vector machine on the iris dataset by splitting the data.
+# Fit a model and compute the score 5 consectuvie times (with different splits each time)
+clf = svm.SVC(kernel="linear", C=1)
+scores = cross_validation.cross_val_score(clf, iris.data, iris.target, cv=5)
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+# ----------------
+# CROSS-VALIDATION ITERATORS
+# ----------------
+
+# KFold divides the samples in k groups of sampes, called folds of equal sizes.
+# The prediction function is learned used k-1 folds and the fold left out is used for test.
+kf = KFold(4, n_folds=2)
+for train, test in kf:
+	print("%s %s" % (train, test))
